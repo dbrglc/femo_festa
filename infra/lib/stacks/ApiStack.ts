0164d194f1e17b5bca2d761cacdbf1fa2e23524a
Gianluca D'Abrosca <gianluca.dabrosca.1999@gmail.com>
2026-04-22 22:18:36 +0200

    remove leaderboard api and add order to bruno

--- infra/lib/stacks/ApiStack.ts
+++ infra/lib/stacks/ApiStack.ts
@@ -15,15 +15,6 @@ export class ApiStack extends Stack {
   constructor(scope: cdk.App, id: string, props: ApiStackProps) {
     super(scope, id, props);
 
-    const apiLambda = new NodejsFunction(this, 'LeaderboardFunction', {
-      runtime: lambda.Runtime.NODEJS_24_X,
-      entry: '../backend/src/getLeaderboard.ts',
-      handler: 'handler',
-      environment: {
-        STAGE: props.stage,
-      },
-    });
-
     const ordersLambda = new NodejsFunction(this, 'SubmitOrderFunction', {
       runtime: lambda.Runtime.NODEJS_24_X,
       entry: '../backend/src/submitOrder.ts',
@@ -36,12 +27,6 @@ export class ApiStack extends Stack {
     const httpApi = new apigwv2.HttpApi(this, 'FemoFestaHttpApi', {
       apiName: `femo-festa-http-${props.stage}`,
       createDefaultStage: true,
-    });
-
-    httpApi.addRoutes({
-      path: '/leaderboard',
-      methods: [apigwv2.HttpMethod.GET],
-      integration: new integrations.HttpLambdaIntegration('LeaderboardIntegration', apiLambda),
     });
 
     httpApi.addRoutes({
